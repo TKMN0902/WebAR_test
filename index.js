7fdbcdbb374f68bf70a0bd243cd96ecec78b16d5
TKMN0902 <65541799+TKMN0902@users.noreply.github.com>
2020-06-02 10:34:50 +0900

    Add files via upload

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ function init() {
 		color: 0x0000ff
 	});
 	const loader = new THREE.TextureLoader();
-	const texture = loader.load('/imgs/earthmap1k.jpg');
+	const texture = loader.load('/imgs/marusohead.png');
 	const material2 = new THREE.MeshStandardMaterial({
 		map: texture
 	});
